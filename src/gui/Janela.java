da6ca1e41c9571ee083d3a563ae4c68a9c449aab
fabio <fabio.henrique.gil@gmail.com>
2016-06-23 17:47:55 -0300

    MELHORIAS NOS METODOS DE CHAMADA

--- src/gui/Janela.java
+++ src/gui/Janela.java
@@ -13,6 +13,8 @@ import javax.swing.*;
 import java.awt.*;
 import static java.awt.Frame.NORMAL;
 import java.awt.event.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -34,10 +36,8 @@ public class Janela extends Frame {
         radio = new Radio();
         fundo = Biblioteca.SonsFundo.SCIENCE_IS_FUN;
         acaoMusicaFundo();
-        //radio.volumeMusicaFundo(3);
-
         criaInterface();
-        radio.tocar(Biblioteca.SonsCurtos.BOAS_VINDAS, 50);
+        inicioMenu();
     }
 
     private void criaInterface() {
@@ -69,48 +69,64 @@ public class Janela extends Frame {
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
 
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.out.println("Execução finalizada.");
+                System.exit(0);
+            }
+        });
+    }
+
+    private void acaoMusicaFundo() {
+        // troca de musica
+        Biblioteca.SonsFundo musicaFundo = (Biblioteca.SonsFundo) fundo;
+        radio.tocar(musicaFundo, 4, true);
+
+    }
+
+    private void inicioMenu() {
+        radio.tocar(Biblioteca.SonsCurtos.BOAS_VINDAS, 50);
+
+        this.addKeyListener(new KeyAdapter() {
+            
+            @Override            
+            public void keyPressed(KeyEvent e) {
+                
+                if (inicioMenu) {
+                    radio.tocar(Biblioteca.SonsCurtos.BUTTON_POSITIVE, 30);
+                    //menu inicial
+
+                    switch (e.getKeyCode()) {
+                        case 10:
+                            //enter
+                            iniciaJogo();
+                            inicioMenu = false;
+                            inicioDeJogo = true;
+                            break;
+                        case 27:
+                            System.exit(0);
+                        case 32:
+                            menuDeOpcoes();
+                            inicioMenu = false;
+                            menuDeOpcoes = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void iniciaJogo() {
+        radio.tocar(Biblioteca.SonsFundo.NENHUMA_MUSICA, NORMAL, true);
+        radio.tocar(Biblioteca.SonsCurtos.START, 50);
         this.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 System.out.println("clicou " + e.getKeyCode());
-                radio.tocar(Biblioteca.SonsCurtos.BUTTON_POSITIVE, 30);
-
-                //menu inicial
-                if (inicioMenu) {
-                    switch (e.getKeyCode()) {
-                        case 10:
-                            //enter
-                            inicioMenu = false;
-                            inicioDeJogo = true;
-                            radio.tocar(Biblioteca.SonsFundo.NENHUMA_MUSICA, NORMAL, true);
-                            radio.tocar(Biblioteca.SonsCurtos.START, 50);
-                            break;
-                        case 27:
-                            //esc
-                            System.exit(0);
-                        case 32:
-                            //back sapace
-                            inicioMenu = false;
-                            menuDeOpcoes = true;
-                            radio.tocar(Biblioteca.SonsCurtos.MENU_DE_OPCOES, 50);
-                            break;
-                        default:
-                            break;
-                    } 
-                }
-
-                //menu de opções
-                if (menuDeOpcoes) {
-                    if (e.getKeyCode() == 10) {//enter
-                        inicioMenu = true;
-                        menuDeOpcoes = false;
-                        radio.tocar(Biblioteca.SonsFundo.SCIENCE_IS_FUN, 3, true);
-                        radio.tocar(Biblioteca.SonsCurtos.BOAS_VINDAS, 50);
-                    }
-
-                }
-
-                //inicio de jogo
                 if (inicioDeJogo) {
                     switch (e.getKeyCode()) {
                         case 37:
@@ -133,35 +149,30 @@ public class Janela extends Frame {
                             break;
                     }
                 }
-
-                //radio.volumeMusicaFundo(3);
             }
-//            @Override
-//            public void keyReleased(KeyEvent e) {
-//                System.out.println("clicou");
-//                radio.tocar(Biblioteca.SonsCurtos.BUTTON_POSITIVE, 30);
-//            }
-//
-//            @Override
-//            public void keyTyped(KeyEvent e) {
-//                System.out.println("clicou");
-//                radio.tocar(Biblioteca.SonsCurtos.BUTTON_POSITIVE, 30);
-//            }
+
         });
 
-        this.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.out.println("Execução finalizada.");
-                System.exit(0);
-            }
-        });
     }
 
-    private void acaoMusicaFundo() {
-        // troca de musica
-        Biblioteca.SonsFundo musicaFundo = (Biblioteca.SonsFundo) fundo;
-        radio.tocar(musicaFundo, 3, true);
+    private void menuDeOpcoes() {
+        radio.tocar(Biblioteca.SonsCurtos.MENU_DE_OPCOES, 50);
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("clicou " + e.getKeyCode());
+                if (menuDeOpcoes) {
+                    if (e.getKeyCode() == 10) {//enter
+                        inicioMenu = true;
+                        menuDeOpcoes = false;
+                        radio.tocar(Biblioteca.SonsFundo.SCIENCE_IS_FUN, 4, true);
+                        radio.tocar(Biblioteca.SonsCurtos.BOAS_VINDAS, 50);
+                    }
+                }
+
+            }
+
+        });
 
     }
 
